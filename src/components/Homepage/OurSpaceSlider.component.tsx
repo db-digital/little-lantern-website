bd416902bf59e645895cea33c9d4ce198ca01d76
Devendra Pratap Singh <59687571+devendradbdigital@users.noreply.github.com>
2025-03-13 13:43:44 +0530

    update slider box

--- src/components/Homepage/OurSpaceSlider.component.tsx
+++ src/components/Homepage/OurSpaceSlider.component.tsx
@@ -1,20 +1,25 @@
 import React from "react";
 import styles from "./OurSpaceSlider.component.module.scss";
 import { Link } from "react-router-dom";
+import ourSpaceSlider from "../../assets/images/our-space-slider-01.png";
 
 const OurSpaceSliderComponent: React.FC = () => {
   return (
     <div className={styles.container}>
       <div className={styles.leftBox}>
-        <h2>Our Space</h2>
+        <h2 className={styles.mainTitle}>Our Space</h2>
         <p>
           At Little Lantern, we have created more than just a preschool—we've
           built a home away from home where children feel safe, inspired, and
           excited to learn.
         </p>
-        <Link to="/">Explore More</Link>
+        <Link to="/" className={styles.exploreBtn}>
+          Explore More
+        </Link>
       </div>
-      <div className={styles.rightBox}></div>
+      <div className={styles.rightBox}>
+        <img src={ourSpaceSlider} alt="" />
+      </div>
     </div>
   );
 };
