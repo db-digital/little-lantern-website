3128fb8ac6df6d6faf62e3c28b51a368b8b31998
Devendra Pratap Singh <59687571+devendradbdigital@users.noreply.github.com>
2025-03-19 21:28:54 +0530

    update slider images on home page

--- src/components/Homepage/OurSpaceSlider.component.tsx
+++ src/components/Homepage/OurSpaceSlider.component.tsx
@@ -1,12 +1,41 @@
 import React, { useRef } from "react";
 import styles from "./OurSpaceSlider.component.module.scss";
 import { Link } from "react-router-dom";
-import ourSpaceSlider from "../../assets/images/our-space-slider-01.png";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Autoplay } from "swiper/modules";
 import { Swiper as SwiperClass } from "swiper"; // Import Swiper type
+import sliderImagOne from "../../assets/images/slider-4-1.jpg";
+import sliderImagTwo from "../../assets/images/slider-4-2.jpg";
+import sliderImagThree from "../../assets/images/slider-4-3.jpg";
+import sliderImagFour from "../../assets/images/slider-4-4.jpg";
+import sliderImagFive from "../../assets/images/slider-4-5.jpg";
+import sliderImagSix from "../../assets/images/slider-4-6.jpg";
+import sliderImagSeven from "../../assets/images/slider-4-7.jpg";
+import sliderImagEight from "../../assets/images/slider-4-8.jpg";
+import sliderImagNine from "../../assets/images/slider-4-9.jpg";
+import sliderImagTen from "../../assets/images/slider-4-10.jpg";
+import sliderImagEleven from "../../assets/images/slider-4-11.jpg";
+import sliderImagTwelve from "../../assets/images/slider-4-12.jpg";
+import sliderImagThirteen from "../../assets/images/slider-4-13.jpg";
+
 import "swiper/css";
 import "swiper/css/autoplay";
+
+const data = [
+  { id: 1, imgUrl: sliderImagOne },
+  { id: 2, imgUrl: sliderImagTwo },
+  { id: 3, imgUrl: sliderImagThree },
+  { id: 4, imgUrl: sliderImagFour },
+  { id: 5, imgUrl: sliderImagFive },
+  { id: 6, imgUrl: sliderImagSix },
+  { id: 7, imgUrl: sliderImagSeven },
+  { id: 8, imgUrl: sliderImagEight },
+  { id: 9, imgUrl: sliderImagNine },
+  { id: 10, imgUrl: sliderImagTen },
+  { id: 11, imgUrl: sliderImagEleven },
+  { id: 12, imgUrl: sliderImagTwelve },
+  { id: 13, imgUrl: sliderImagThirteen },
+];
 
 const OurSpaceSliderComponent: React.FC = () => {
   const swiperRef = useRef<SwiperClass | null>(null); // Type for Swiper instance
@@ -41,12 +70,12 @@ const OurSpaceSliderComponent: React.FC = () => {
           modules={[Autoplay]}
           onSwiper={(swiper: SwiperClass) => (swiperRef.current = swiper)} // Store Swiper instance
         >
-          <SwiperSlide>
-            <img src={ourSpaceSlider} alt="" />
-          </SwiperSlide>
-          <SwiperSlide>
-            <img src={ourSpaceSlider} alt="" />
-          </SwiperSlide>
+          {data &&
+            data.map((item) => (
+              <SwiperSlide key={item.id}>
+                <img className={styles.sliderImag} src={item.imgUrl} alt="" />
+              </SwiperSlide>
+            ))}
         </Swiper>
       </div>
     </div>
